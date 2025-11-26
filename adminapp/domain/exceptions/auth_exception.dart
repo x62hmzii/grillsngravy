1350cca91152b6b6289b7f4719cf6c9d30fe7dc5
Hamza <hamza74salman@gmail.com>
2025-11-26 10:18:22 +0500

    update

--- adminapp/domain/exceptions/auth_exception.dart
+++
@@ -1,9 +0,0 @@
-class AuthException implements Exception {
-  final String message;
-  final String code;
-
-  AuthException(this.message, {this.code = 'unknown'});
-
-  @override
-  String toString() => 'AuthException: $message';
-}
